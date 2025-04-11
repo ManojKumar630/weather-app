21272a82fe23ec1dffa8d60b2c6c928712419fb0
ManojKumar630 <manojkumarkondaparthii@gmail.com>
2025-04-11 10:26:52 +0530

    Update weather.js

--- routes/weather.js
+++ routes/weather.js
@@ -8,7 +8,7 @@ const axios = require("axios");
 router.get("/", async (req, res) => {
   const city = req.query.city || "Hyderabad";
   const apiKey =13df8dcd69f58e21940196c94e1b4cb5; 
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=13df8dcd69f58e21940196c94e1b4cb5&units=metric`;
 
   try {
     const response = await axios.get(apiUrl);
