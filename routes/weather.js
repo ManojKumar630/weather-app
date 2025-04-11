80c1da55db111835be8baa292491483b8442e772
ManojKumar630 <manojkumarkondaparthii@gmail.com>
2025-04-11 10:03:23 +0530

    Update weather.js

--- routes/weather.js
+++ routes/weather.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
-require('dotenv').config();
+
 
 // Weather Route
 router.get("/", async (req, res) => {
